da83da23174cd12a23d8c3de9d8b06e493119572
tjortiz17 <tjortiz17@gmail.com>
2019-11-06 19:21:01 -0800

    add Atmel Studio project files

---
+++ PropellerLEDDisplay/main.c
@@ -0,0 +1,18 @@
+/*
+ * PropellerLEDDisplay.c
+ *
+ * Created: 11/6/2019 7:19:19 PM
+ * Author : tjort
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
